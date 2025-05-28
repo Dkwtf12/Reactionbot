b5d06539636db329cfcfac83f337544966f553d0
Dkwtf12 <121540365+Dkwtf12@users.noreply.github.com>
2025-05-28 20:23:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-OWNER_ID = int(os.getenv("OWNER_ID"))
+BOT_TOKEN = os.getenv("7458126832:AAG_0NaABm66VsE0av2b_EfZ2C1E0xsTroc")
+API_ID = int(os.getenv("19977673"))
+API_HASH = os.getenv("f75386c7aab88e2ad9b5de220fc0ceb4")
+OWNER_ID = int(os.getenv("1568895149"))
